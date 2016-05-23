d0ce5b15d7f65419745bbdb247ae85d005eb9be8
Avindra Goolcharan <Avindra.Goolcharan@adp.com>
2016-05-23 13:33:57 -0400

    Tests: add cases for implementations of native php interfaces

--- test/TNSParserTest.php
+++ test/TNSParserTest.php
@@ -4,6 +4,8 @@ use DCarbone\TNSParser;
 class TNSParserTest extends \PHPUnit_Framework_TestCase
 {
 
+    private static $totalNumEntries = 2;
+
     public function setUp()
     {
         $this->sampleFile = __DIR__ . '/fixtures/sampletnsnames.ora';
@@ -20,7 +22,7 @@ class TNSParserTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('tigercloud.aws.cloud02.local', $tiger);
     }
 
-    public function testSerializationFormat()
+    public function testJSONSerialization()
     {
         $data = $this->parser->jsonSerialize();
 
@@ -31,10 +33,18 @@ class TNSParserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($data['SCOTT']['DESCRIPTION']['ADDRESS_LIST']['ADDRESS']['PORT'], "1521");
 
         // should only be two entries
-        $this->assertEquals(count($data), 2);
+        $this->assertEquals(count($data), self::$totalNumEntries);
     }
 
+    public function testSerialization()
+    {
+        $expectedSerialization = 'C:18:"DCarbone\TNSParser":884:{a:3:{i:0;a:2:{s:5:"SCOTT";a:1:{s:11:"DESCRIPTION";a:2:{s:12:"ADDRESS_LIST";a:3:{s:12:"LOAD_BALANCE";s:3:"off";s:8:"FAILOVER";s:2:"ON";s:7:"ADDRESS";a:3:{s:8:"PROTOCOL";s:3:"TCP";s:4:"HOST";s:28:"scottcloud.aws.cloud03.local";s:4:"PORT";s:4:"1521";}}s:12:"CONNECT_DATA";a:1:{s:12:"service_name";s:5:"SCOTT";}}}s:5:"TIGER";a:1:{s:11:"DESCRIPTION";a:2:{s:12:"ADDRESS_LIST";a:3:{s:12:"LOAD_BALANCE";s:3:"off";s:8:"FAILOVER";s:2:"ON";s:7:"ADDRESS";a:3:{s:8:"PROTOCOL";s:3:"TCP";s:4:"HOST";s:28:"tigercloud.aws.cloud02.local";s:4:"PORT";s:4:"1521";}}s:12:"CONNECT_DATA";a:1:{s:12:"service_name";s:5:"TIGER";}}}}i:1;a:2:{s:5:"SCOTT";a:6:{i:0;s:3:"off";i:1;s:2:"ON";i:2;s:3:"TCP";i:3;s:28:"scottcloud.aws.cloud03.local";i:4;s:4:"1521";i:5;s:5:"SCOTT";}s:5:"TIGER";a:6:{i:0;s:3:"off";i:1;s:2:"ON";i:2;s:3:"TCP";i:3;s:28:"tigercloud.aws.cloud02.local";i:4;s:4:"1521";i:5;s:5:"TIGER";}}i:2;b:0;}}';
+        $actualSerialization = serialize($this->parser);
+        $this->assertEquals($expectedSerialization, $actualSerialization);
 
+        $reconstructed = unserialize($expectedSerialization);
+        $this->_conductSearchTest($reconstructed);
+    }
 
     public function testInvalidEntry()
     {
@@ -42,13 +52,65 @@ class TNSParserTest extends \PHPUnit_Framework_TestCase
         $this->assertNull($badEntry);
     }
 
-    public function testSearch()
+    private function _conductSearchTest($root)
     {
-        $entries = $this->parser->search('cloud03');
+        $entries = $root->search('cloud03');
         $this->assertEquals($entries, ['SCOTT']);
 
         // check that full TNS can be resolved from $parser
-        $fullTNS = $this->parser[$entries[0]];
+        $fullTNS = $root[$entries[0]];
         $this->assertEquals($fullTNS['DESCRIPTION']['ADDRESS_LIST']['ADDRESS']['PROTOCOL'], 'TCP');
+    }
+
+    public function testSearch()
+    {
+        $this->_conductSearchTest($this->parser);
+    }
+
+    public function testCountable()
+    {
+        $this->assertEquals(count($this->parser), self::$totalNumEntries);
+    }
+
+    public function testArrayAccess()
+    {
+        $entry = $this->parser['SCOTT'];
+        $this->assertEquals($entry['DESCRIPTION']['CONNECT_DATA']['service_name'], 'SCOTT');
+
+        $badKey = 'THISDOESNTEXIST';
+        try {
+            $invalidEntry = $this->parser[$badKey];
+        } catch(\OutOfRangeException $e) {
+            $this->assertEquals("No key ${badKey} exists", $e->getMessage());
+        }
+
+        try {
+            unset($this->parser['SCOTT']);
+        } catch(\BadMethodCallException $e) {
+            $this->assertEquals('Not allowed to unset values on this object', $e->getMessage());
+        }
+
+        try {
+            $this->parser['SCOTT'] = 'Testing override ability';
+        } catch(\BadMethodCallException $e) {
+            $this->assertEquals('Not allowed to set values on this object', $e->getMessage());
+        }
+
+        $this->assertTrue(isset($this->parser['SCOTT']));
+        $this->assertFalse(isset($this->parser['JOHN']));
+    }
+
+    public function testIteration()
+    {
+        $total = 0;
+        $entries = [];
+        $keys = [];
+        foreach ($this->parser as $key => $entry) {
+            ++$total;
+            $entries []= $entry;
+            $keys []= $key;
+        }
+        $this->assertEquals($keys, ['SCOTT', 'TIGER']);
+        $this->assertEquals($total, self::$totalNumEntries);
     }
 }
